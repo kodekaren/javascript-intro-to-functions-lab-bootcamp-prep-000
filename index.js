3fc78b4a82642c1f021ad5213442fecdb8caa42b
karen ercoli <karenercoli@gmail.com>
2018-10-30 17:47:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,10 +13,20 @@ function logShout(string) {
 function logWhisper(string) {
   console.log("hello")
 }
-function string(string) {
-  return("I can't hear you!")
-}
 
 function sayHiToGrandma(string) {
   return string()
 }
+var uppercase = "HELLO!"
+ 
+uppercase.toUpperCase() === uppercase // true
+ 
+var lowercase = 'hello!'
+ 
+lowercase.toLowerCase() === lowercase // true
+ 
+var mixedCase = 'Hi there!'
+ 
+mixedCase.toLowerCase() === mixedCase // false
+ 
+mixedCase.toUpperCase() === mixedCase // false
