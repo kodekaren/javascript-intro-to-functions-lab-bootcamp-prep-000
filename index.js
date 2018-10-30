ca565a75b1e34915d68b998065b453b429abee16
karen ercoli <karenercoli@gmail.com>
2018-10-30 18:23:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,11 +14,3 @@ function logWhisper(string) {
   console.log().ToLowerCase
 }
 
-var uppercase = "HELLO"
-uppercase.ToUpperCase() === uppercase
-
-function sayHiToGrandma(string) {
-  if ((string)===uppercase) {
-    return 'YES INDEED!'
-  }
-  
