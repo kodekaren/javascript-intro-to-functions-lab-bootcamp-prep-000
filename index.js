5e9537492418b5b589837e1d8b7887c41d928108
karen ercoli <karenercoli@gmail.com>
2018-10-30 18:23:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,18 +14,11 @@ function logWhisper(string) {
   console.log().ToLowerCase
 }
 
+var uppercase = "HELLO"
+uppercase.ToUpperCase() === uppercase
 
 function sayHiToGrandma(string) {
   if ((string)===uppercase) {
     return 'YES INDEED!'
   }
-  else if ((string)===lowercase) {
-    return "I can't hear you!"
-  }
-  else if ((string)==="I love you, Grandma.") {
-    return "I love you, too."
-  }
-  }
-"hello"
-"I love you, Grandma."
-"HELLO"
+  
