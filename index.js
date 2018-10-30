31f4c02b438183a560b1394ce553371bfff70880
karen ercoli <karenercoli@gmail.com>
2018-10-30 18:24:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-function shout(string) {
-  return string.toUpperCase()
+var uppercase, lowercase
+ 
+function shout(string){
+  uppercase = string.toUpperCase();
+  string = uppercase;
+  return uppercase;
 }
-"Hello"
-
-function whisper(string) {
-  return string.toLowerCase()
+function whisper(string){
+  lowercase = string.toLowerCase();
+  string = lowercase;
+  return lowercase;
 }
-"HELLO"
-function logShout(string) {
-  console.log().toUpperCase
+function logShout(string){
+  uppercase = string.toUpperCase()
+  string = uppercase;
+  console.log(uppercase);
 }
-function logWhisper(string) {
-  console.log().ToLowerCase
+function logWhisper(string){
+  lowercase = string.toLowerCase()
+  string = uppercase;
+  console.log(lowercase);
+}
+function sayHiToGrandma(string){
+    if (string.toLowerCase() === string){
+      return 'I can\'t hear you!';
+    }
+    if (string.toUpperCase() === string){
+      return 'YES INDEED!';
+    }
+    if (string === 'I love you, Grandma.'){
+      return 'I love you, too.';
+    }
 }
