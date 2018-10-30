bc7ec4970d8b8f90fbea5c24b70033bc98fd935d
karen ercoli <karenercoli@gmail.com>
2018-10-30 18:00:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  return "I can't hear you".toLowerCase()
+  return "HELLO".toLowerCase()
 }
 "I can't hear you!"
+"I love you, Grandma"
